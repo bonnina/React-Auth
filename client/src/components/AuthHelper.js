d83ef4497503b23cf36729e229a013deb31a5c10
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-01 22:33:24 +0200

    Create auth helper methods

---
+++ client/src/components/AuthHelper.js
@@ -0,0 +1,80 @@
+import decode from 'jwt-decode'
+
+export default class AuthHelper {
+ 
+  login = (username, password) => {
+    return this.fetch(`/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password
+      })
+    }).then(res => {
+      this.setToken(res.token); 
+      return Promise.resolve(res);
+    });
+  };
+
+  loggedIn = () => {
+    const token = this.getToken(); 
+    return !!token && !this.isTokenExpired(token); 
+  };
+
+  isTokenExpired = token => {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      console.log("token expired");
+      return false;
+    }
+  };
+
+  setToken = tkn => {
+    localStorage.setItem("token", tkn);
+  };
+
+  getToken = () => {
+    return localStorage.getItem("token");
+  };
+
+  logout = () => {
+    localStorage.removeItem("token");
+  };
+
+  decodeTkn = () => {
+    let answer = decode(this.getToken());
+    console.log("Received answer!");
+    return answer;
+  };
+
+  fetch = (url, options) => {
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    };
+
+    if (this.loggedIn()) {
+      headers["Authorization"] = "Bearer " + this.getToken();
+    }
+
+    return fetch(url, {
+      headers,
+      ...options
+    })
+      .then(this.checkStatus)
+      .then(response => response.json());
+  };
+
+  checkStatus = res => {
+    if (res.status >= 200 && res.status < 300) {
+      return res;
+    } else {
+      let error = new Error(res.statusText);
+      error.response = res;
+      throw error;
+    }
+  };
+}
