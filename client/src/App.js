44cd082a25cc03106a0c58565cc936800fc107d9
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-01 22:37:50 +0200

    Update App.js to use AuthHelper and withAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AuthHelper from './components/AuthHelper';
+import withAuth from './components/withAuth';
 
-class App extends Component {
+class App extends React.Component {
+  Auth = new AuthHelper();
+  state = {
+    username: "",
+    password: ""
+  }
+
+  handleLogout = () => {
+    this.Auth.logout()
+    this.props.history.replace('/login');
+  }
+
   render() {
+    let name = this.props.confirm.username;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="main-page">
+          <div className="top-section">
+            <h1>Welcome, {name}</h1>
+          </div>
+          <div className="bottom-section">
+            <button onClick={this.handleLogout}> LOG OUT </button>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withAuth(App);
