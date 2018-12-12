eb6591d590b5fed92421ef9e60b476c2564ae99e
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-12 16:58:14 +0200

    Add route protection (access for auth-ed users only) to / route

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,14 @@ import App from './App';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import FacebookAuthRedirect from './components/FacebookAuthRedirect';
+import withAuth from './components/withAuth';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 <BrowserRouter>
   <div>
-    <Route exact path="/" component={App} />
+    <Route exact path="/" component={ withAuth(App) } />
     <Route exact path="/login" component={Login} />
     <Route exact path="/signup" component={Signup} />
     <Route exact path="/socialauthredirect/" component={FacebookAuthRedirect} />
